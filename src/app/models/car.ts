28072ef8ae0015e9fb1da69450c5729b30fdb94d
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2021-03-28 18:20:02 +0300

    Gün 17 - Ödev

---
+++ src/app/models/car.ts
@@ -0,0 +1,8 @@
+export interface Car {
+    id:number,
+    brand:string,
+    color:string,
+    model:string,
+    modelYear:number,
+    dailyPrice:number
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,10 @@
+export interface Rental {
+    id:number,
+    carBrand:string,
+    carModel:string,
+    customerName:string,
+    customerSurname:string,
+    customerCompanyName:string,
+    rentDate:Date,
+    returnDate:Date
+}
